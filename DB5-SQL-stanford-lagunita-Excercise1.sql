9c50eaea4a761fd003025f8c80534629f8181cc0
Carrie990 <33357231+Carrie990@users.noreply.github.com>
2018-05-02 22:30:46 -0700

    Update DB5-SQL-stanford-lagunita-Excercise1.sql

--- DB5-SQL-stanford-lagunita-Excercise1.sql
+++ DB5-SQL-stanford-lagunita-Excercise1.sql
@@ -2,13 +2,13 @@
 
 # Excercise1: SQL Movie-Rating Query Exercises
 
-Q1. Find the titles of all movies directed by Steven Spielberg. 
+-- Q1. Find the titles of all movies directed by Steven Spielberg. 
 ---
   select title
   from Movie
   where director = 'Steven Spielberg'
 
-Q2. Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. 
+-- Q2. Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. 
 ---
   SELECT DISTINCT year
   FROM Movie
@@ -19,7 +19,7 @@ Q2. Find all years that have a movie that received a rating of 4 or 5, and sort 
   ) 
   ORDER BY year
 
-Q3. Find the titles of all movies that have no ratings. 
+-- Q3. Find the titles of all movies that have no ratings. 
 ---
   select title
   from Movie
@@ -28,7 +28,7 @@ Q3. Find the titles of all movies that have no ratings.
   from Rating
   )
 
-Q4. Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. 
+-- Q4. Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. 
 ---
   SELECT DISTINCT name
   FROM Reviewer
@@ -38,7 +38,7 @@ Q4. Some reviewers didn't provide a date with their rating. Find the names of al
      WHERE ratingDate is NULL
     )
 
-Q5. Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
+-- Q5. Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
 ---
   SELECT Reviewer.name, Movie.title, Rating.stars, Rating.ratingDate 
   FROM Reviewer, Movie, Rating
@@ -46,7 +46,7 @@ Q5. Write a query to return the ratings data in a more readable format: reviewer
   AND Rating.mID = Movie.mID
   GROUP BY Reviewer.name, Movie.title, Rating.stars
 
-Q6. For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. 
+-- Q6. For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. 
 ---
   select name, title
   from Movie, Reviewer,
@@ -60,7 +60,7 @@ Q6. For all cases where the same reviewer rated the same movie twice and gave it
   where Movie.mID = MR.mID
       and Reviewer.rID = MR.rID
 
-Q7. For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. 
+-- Q7. For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. 
 ---
   select title, star
   from Movie,
@@ -71,7 +71,7 @@ Q7. For each movie that has at least one rating, find the highest number of star
   where Movie.mID = Ra.mID
   order by title
   
-Q8. For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
+-- Q8. For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
 ---
   select title,ratingspread
   from Movie,
@@ -82,7 +82,7 @@ Q8. For each movie, return the title and the 'rating spread', that is, the diffe
    where Movie.mID = RS.mID
    order by ratingspread desc,title
    
-Q9. Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
+-- Q9. Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
 ---
   select avgold-avgnew difference
   from
