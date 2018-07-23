00e246c5b5715f052343afb581a942e8212aa825
Carrie990 <33357231+Carrie990@users.noreply.github.com>
2018-07-23 16:52:02 -0700

    Update LeetCode-TeraData-20180723.sql

--- LeetCode-TeraData-20180723.sql
+++ LeetCode-TeraData-20180723.sql
@@ -52,27 +52,63 @@ GROUP BY Company
 -- 579. Find Cumulative Salary of an Employee 
 -- https://leetcode.com/articles/find-cumulative-salary-of-an-employee/
 -- | Id | Month | Salary |
+                           
+-- MySQL
+SELECT r1.ID, r1.Month, SUM(r2.Salary) AS Salary
+FROM
+    (SELECT Id, Month, Salary, @rnk := @rnk+1
+    FROM Employee,
+        (SELECT @rnk := 0) r
+    GROUP BY Id
+    ) r1, 
+    (SELECT Id, Month, Salary, @rnk := @rnk+1
+    FROM Employee,
+        (SELECT @rnk := 0) r
+    GROUP BY Id
+    ) r2
+WHERE r1.rnk in (2,3,4)
+    AND r2.rnk IN (2,3,4)
+    AND r2.rnk >= r1.rnk
+GROUP BY r1.ID
+ORDER BY Id, Month DESC
+
 -- TeraData
 WITH Ranking AS(
-    SELECT Id, Month, Salary, RANK() OVER (ORDER BY Month DESC) AS rnk
+    SELECT Id, Month, Salary, RANK() OVER (PARTITION BY Id ORDER BY Month DESC) AS rnk
     FROM Employee
-    WHERE rnk in (2,3,4)
     ORDER BY ID, rnk
 )
-SELECT Id, Month,
+SELECT Id, Month, SUM(r2.Salary) AS Salary
+FROM Ranking r1,Ranking r2
+WHERE r1.rnk in (2,3,4)
+    AND r2.rnk in (2,3,4)
+    AND r2.rnk >= r1.rnk
+GROUP BY Id
+ORDER BY Id, Month DESC                         
+
+-- 615. Average Salary: Departments VS Company
+-- https://leetcode.com/articles/average-salary-departments-vs-company/
+-- | id | employee_id | amount | pay_date   |,| employee_id | department_id |,| pay_month | department_id | comparison  |
+-- TeraData
+WITH JoinSalary AS(
+    SELECT id, salary.employee_id, amount, SUBSTR(pay_date,1,7) AS pay_month, department_id
+    FROM salary,employee
+    WHERE salary.employee_id = employee.employee_id
+), AvgCom AS(
+    SELECT pay_month, AVG(amount) AS avgcom
+    FROM JoinSalary
+    GROUP BY pay_month
+), AvgDept AS(
+    SELECT department_id,pay_month, AVG(amount) AS avgdept
+    FROM JoinSalary
+    GROUP BY department_id, pay_month
+)
+SELECT AvgDept.pay_month, department_id, 
     CASE
-        WHEN rnk = 2 THEN (SELECT SUM(Salary)
-                          FROM (SELECT Salary
-                                FROM Ranking) t1
-                           )
-        WHEN rnk = 3 THEN (SELECT SUM(Salary)
-                          FROM (SELECT Salary
-                                FROM Ranking
-                                WHERE rnk in (3,4)) t2
-        ELSE (SELECT Salary
-              FROM Ranking
-              WHERE rnk = 4             
-              )
-    END
-FROM Ranking
-ORDER BY Id, Month DESC
+        WHEN avgdept > avgcom THEN 'higher'
+        WHEN avgdept < avgcom THEN 'lower'
+        ELSE 'same'
+    END AS comparison
+FROM AvgDept,AvgCom
+WHERE AvgDept.pay_month = AvgCom.pay_month
+ORDER BY AvgDept.pay_month DESC, department_id
