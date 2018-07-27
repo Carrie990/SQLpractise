1a8a91d79d4f7311510b9ef09c278e2330408741
Carrie990 <33357231+Carrie990@users.noreply.github.com>
2018-07-27 15:38:33 -0700

    Update LeetCode-TeraData-20180723.sql

--- LeetCode-TeraData-20180723.sql
+++ LeetCode-TeraData-20180723.sql
@@ -4,28 +4,19 @@
 -- |Id   | Company    | Salary |
 -- MySQL
 
-SELECT 
-    Id, Company, Salary
+SELECT
+    Employee.Id, Employee.Company, Employee.Salary
 FROM
-    (SELECT 
-        e.Id,
-            e.Salary,
-            e.Company,
-            IF(@prev = e.Company, @Rank:=@Rank + 1, @Rank:=1) AS rank,
-            @prev:=e.Company
-    FROM
-        Employee e, (SELECT @Rank:=0, @prev:=0) AS temp
-    ORDER BY e.Company , e.Salary , e.Id) Ranking
-        INNER JOIN
-    (SELECT 
-        COUNT(*) AS totalcount, Company AS name
-    FROM
-        Employee e2
-    GROUP BY e2.Company) companycount ON companycount.name = Ranking.Company
+    Employee,
+    Employee alias
 WHERE
-    Rank = FLOOR((totalcount + 1) / 2)
-        OR Rank = FLOOR((totalcount + 2) / 2)
-;
+    Employee.Company = alias.Company
+GROUP BY Employee.Company , Employee.Salary
+HAVING SUM(CASE
+    WHEN Employee.Salary = alias.Salary THEN 1
+    ELSE 0
+END) >= ABS(SUM(SIGN(Employee.Salary - alias.Salary)))
+ORDER BY Employee.Id
 
 -- TeraData (?)
 WITH rank AS (
@@ -39,8 +30,8 @@ WITH rank AS (
 SELECT Id, rank.Company, Salary
 FROM rank,cnt 
 WHERE rank.Company = cnt.Company
-    rank = (cnt+1)/2
-    OR rank = (cnt+2)/2
+    AND (rank = (cnt+1)/2
+         OR rank = (cnt+2)/2)
 
 
 -- 579. Find Cumulative Salary of an Employee 
