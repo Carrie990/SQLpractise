12328d75a3859e542d53d5eee20585326ba8ca92
Carrie990 <33357231+Carrie990@users.noreply.github.com>
2018-09-12 17:33:18 -0700

    Update Teradata_mark.sql

--- Teradata_mark.sql
+++ Teradata_mark.sql
@@ -8,4 +8,52 @@
   FROM table
   QUALIFY (rnk>10 and rnk<21)
 
-  
+-- Q2. CREATE TABLE
+CREATE SET TABLE EMPLOYEE,FALLBACK ( 
+   EmployeeNo INTEGER, 
+   FirstName VARCHAR(30), 
+   LastName VARCHAR(30), 
+   DOB DATE FORMAT 'YYYY-MM-DD', 
+   JoinedDate DATE FORMAT 'YYYY-MM-DD', 
+   DepartmentNo BYTEINT 
+) 
+UNIQUE PRIMARY INDEX ( EmployeeNo );
+
+-- Q3. sets manipulation
+-- intersect
+SELECT col1, col2, col3… 
+FROM  
+<table 1>
+[WHERE condition] 
+INTERSECT 
+
+SELECT col1, col2, col3… 
+FROM  
+<table 2> 
+[WHERE condition];
+
+-- MINUS/EXCEPT
+SELECT EmployeeNo 
+FROM  
+Employee 
+MINUS 
+
+SELECT EmployeeNo 
+FROM  
+Salary;
+
+-- SUBSTRING/||
+String Function	Result
+SELECT SUBSTRING(‘warehouse’ FROM 1 FOR 4)	ware
+SELECT SUBSTR(‘warehouse’,1,4)	ware
+SELECT ‘data’ || ‘ ‘ || ‘warehouse’	data warehouse
+SELECT UPPER(‘data’)	DATA
+SELECT LOWER(‘DATA’)	data
+
+--  EXTRACT
+SELECT EXTRACT(YEAR FROM CURRENT_DATE);  
+
+-- DATE
+SELECT CURRENT_DATE, CURRENT_DATE + INTERVAL '03' YEAR; 
+
+
